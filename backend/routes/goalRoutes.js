fdfadc1240307b4ebfaea1d11480d6329207977c
Michael Eleyowo <michaeleleyowo29@gmail.com>
2022-04-25 08:17:30 +0100

    deleted goals

--- backend/routes/goalRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getGoals, setGoals, updateGoals, deleteGoals } = require("../controllers/goalController");
-const router = express.Router();
-const { protect } = require("../middleware/authMiddleware");
-
-router.route("/").get(protect, getGoals).post(protect, setGoals);
-router.route("/:id").delete(protect, deleteGoals).put(protect, updateGoals);
-
-module.exports = router;
